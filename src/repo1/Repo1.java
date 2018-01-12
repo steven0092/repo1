e6d31aab5894a307179b77b7a3f12718d3dfa696
xp <xp@CETYSDAM023>
2018-01-12 19:17:22 +0100

    He añadido un system.java

--- src/repo1/Repo1.java
+++ src/repo1/Repo1.java
@@ -17,6 +17,8 @@ public class Repo1 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hola mundo!");
+        System.out.println("Hola Juan ");
+        System.out.println("Hola Steven");
     
         
     }
